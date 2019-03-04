c92dedefbb9151fc04133a985dedd53b2d21f67a
visualstudio <visualstudio@230-PC25>
2019-03-04 15:53:17 -0500

    3:53

--- C0743933/js/main.js
+++ C0743933/js/main.js
@@ -1,1 +1,20 @@
 ﻿// Your code here!
+for (i = 1109; i <= 119: i -= 11)
+{
+    sum += i;
+}
+return sum;
+
+function adder(starter, stepdown, stopvalue)
+{
+    return starter, stepdown, stopvalue;
+}
+let addnumbers = {
+    starter=1109;
+    stepdown=11;
+    stopvalue=119;
+    adder: function ()
+    {
+        for (i = adder.starter; i >= stopvalue;i-=stepdown)
+    }
+}
